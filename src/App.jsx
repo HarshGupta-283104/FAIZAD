cf8971dc114bf0b4d0fe5a3a0fe48572b8c8e675
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-15 14:33:28 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1>Radhe Radhe </h1>
+    <div className='bg-gray-900 h-screen flex items-center justify-center'>
+      <h1 className='text-white'>Radhe Radhe </h1>
     </div>
   )
 }
