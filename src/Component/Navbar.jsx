6dad12b7ef653a7bf195d123b8796cc7a6082014
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-15 15:34:02 +0530

    Navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import {navItems} from './index'
 function Navbar() {
   return (
     <div>
-      <h1>NavBar</h1>
+      {navItems.map((item, index) => (
+        <div key={index} className=' w-full flex items-center justify-between p-4 bg-gray-800 text-white'>
+          <img src={item.icon} alt={item.name} className='w-6 h-6' />
+          <span className='ml-2'>{item.name}</span>
+        </div>
+      ))}
     </div>
   )
 }
