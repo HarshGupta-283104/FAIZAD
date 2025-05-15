528f36bff254016cf975b90cc894fbd7fdf8a0ea
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-15 15:17:14 +0530

    utils-commit

---
+++ src/assets/Image/index.js
@@ -0,0 +1,73 @@
+
+import Iphone1 from './1Iphone.png';
+import Iphone2 from './2Iphone.png';
+import Iphone2_2 from './2Iphone2.png';
+import BillBoard from './BillBoard.png';
+import BookStack from './BookStack.png';
+import BrandStack from './BrandStack.png';
+import Buddy from './Buddy.png';
+import BusStop from './BusStop.png';
+import Cabin from './Cabin.png';
+import CallPc from './CallPc.png';
+import Car from './Car.png';
+import Cards from './Cards.png';
+import Cat from './Cat.png';
+import Cat2 from './Cat2.png';
+import Cat3 from './Cat3.png';
+import Celero from './Celero.png';
+import ComicalEstate from './ComicalEstate.png';
+import Desk from './Desk.png';
+import DoubleQuote from './DoubleQuote.png';
+import GridTypography from './GridTypography.png';
+import GuidlinesStack from './GuidlinesStack.png';
+import HatypoMobileWhite from './HatypoMobileWhite.png';
+import HatypoPc from './HatypoPc.png';
+import HatypoPc2 from './HatypoPc2.png';
+import HoldPhone from './HoldPhone.png';
+import Homical from './Homical.png';
+import HomicalLogo from './HomicalLogo.png';
+import IdCard from './IdCard.png';
+import IphoneCorner from './IphoneCorner.png';
+import OurServicePc from './OurServicePc.png';
+import PcChair from './PcChair.png';
+import Standing from './Standing.png';
+import Status from './Status.png';
+import VenAI from './VenAI.png';
+
+
+export {
+  Iphone1,
+  Iphone2,
+  Iphone2_2,
+  BillBoard,
+  BookStack,
+  BrandStack,
+  Buddy,
+  BusStop,
+  Cabin,
+  CallPc,
+  Car,
+  Cards,
+  Cat,
+  Cat2,
+  Cat3,
+  Celero,
+  ComicalEstate,
+  Desk,
+  DoubleQuote,
+  GridTypography,
+  GuidlinesStack,
+  HatypoMobileWhite,
+  HatypoPc,
+  HatypoPc2,
+  HoldPhone,
+  Homical,
+  HomicalLogo,
+  IdCard,
+  IphoneCorner,
+  OurServicePc,
+  PcChair,
+  Standing,
+  Status,
+  VenAI
+};
