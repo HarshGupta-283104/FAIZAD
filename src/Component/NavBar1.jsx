7fefc8d56a120b7921c6e469b8b4d9c2661425ad
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-16 10:13:47 +0530

    Some changes

--- src/Component/NavBar1.jsx
+++ src/Component/NavBar1.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
-import logo_light from '../../src/assets/NavBarAssets/'
-// import logo_dark from 'assets/NavBarAssets/logo_white.png'
+import { LogoWhite, LogoBlack, Day, Night, SearchDark, SearchLight } from '../assets/NavBarAssets/NavItems'
+
 
 function NavBar1() {
+    const NavItems = ['Home', 'Product', 'Features', 'About'];
+    let spanId = document.getElementById("#span0");
+    console.log(spanId);
+    
   return (
-    <div>
-        <img src="" alt="logo" />
-        <ul>
-            <li>Home</li>
-            <li>Product</li>
-            <li>Features</li>
-            <li>About</li>
+    <div className='w-full flex items-center justify-between backdrop:blur-2xl bg-white/80 px-10 py-5 px-4'>
+        <img src={LogoBlack} alt="logo" className='w-[200px] cursor-pointer'/>
+        <ul className='flex items-center gap-7 text-2xl font-[Regular] text-gray-800 gap-15 cursor-pointer'>
+            {NavItems.map((items,index)=>(
+             <li key={index} ><span id={"span"+index}>
+            {items}
+                </span></li>   
+            ))}
         </ul>
-        <div className=''>
-            <input type="text" placeholder='Search' />
-            <img src="" alt="search" />
+        <div className='flex items-center gap-5 relative right-1/8'>
+            <input type="text" placeholder='Search' className='border-2 rounded-xl p-1 text-center' />
+            <img src={SearchDark} alt="search" className='w-8 h-8 cursor-pointer' />
 
+        <img src={Night} alt="toggle-icon" className='w-8 h-8 cursor-pointer '/>
         </div>
-        <img src="" alt="toggle-icon" />
     </div>
   )
 }
---
+++ src/assets/NavBarAssets/NavItems.js
@@ -0,0 +1,15 @@
+import LogoBlack from './logo-black.png';
+import LogoWhite from './logo-white.png';
+import Day from './day.png';
+import Night from './night.png';
+import SearchDark from './search-b.png';
+import SearchLight from './search-w.png';
+
+export {
+  LogoBlack,
+  LogoWhite,
+  Day,
+  Night,
+  SearchDark,
+  SearchLight
+};
