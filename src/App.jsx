c15c7402585fc67cc560b58557fd46041d6f430c
harsh-gupta283104 <harsh.gupta332694@gmail.com>
2025-05-27 21:55:15 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Navbar from './Component/NavBar1'
 import Hero from './Component/Hero'
+import { CardMarqueeData } from './Component'
 function App() {
+
+  console.log(CardMarqueeData);
+  
   return (
 
     <div className='w-full h-full flex flex-col items-center justify-center'>
-      <Navbar />
       <Hero />
     </div>
   )
---
+++ src/Component/Utills.js
@@ -0,0 +1,73 @@
+
+import Iphone1 from '/assets/Image/1Iphone.png';
+import Iphone2 from '/assets/Image/2Iphone.png';
+import Iphone2_2 from '/assets/Image/2Iphone2.png';
+import BillBoard from '/assets/Image/BillBoard.png';
+import BookStack from '/assets/Image/BookStack.png';
+import BrandStack from '/assets/Image/BrandStack.png';
+import Buddy from '/assets/Image/Buddy.png';
+import BusStop from '/assets/Image/BusStop.png';
+import Cabin from '/assets/Image/Cabin.png';
+import CallPc from '/assets/Image/CallPc.png';
+import Car from '/assets/Image/Car.png';
+import Cards from '/assets/Image/Cards.png';
+import Cat from '/assets/Image/Cat.png';
+import Cat2 from '/assets/Image/Cat2.png';
+import Cat3 from '/assets/Image/Cat3.png';
+import Celero from '/assets/Image/Celero.png';
+import ComicalEstate from '/assets/Image/ComicalEstate.png';
+import Desk from '/assets/Image/Desk.png';
+import DoubleQuote from '/assets/Image/DoubleQuote.png';
+import GridTypography from '/assets/Image/GridTypography.png';
+import GuidlinesStack from '/assets/Image/GuidlinesStack.png';
+import HatypoMobileWhite from '/assets/Image/HatypoMobileWhite.png';
+import HatypoPc from '/assets/Image/HatypoPc.png';
+import HatypoPc2 from '/assets/Image/HatypoPc2.png';
+import HoldPhone from '/assets/Image/HoldPhone.png';
+import Homical from '/assets/Image/Homical.png';
+import HomicalLogo from '/assets/Image/HomicalLogo.png';
+import IdCard from '/assets/Image/IdCard.png';
+import IphoneCorner from '/assets/Image/IphoneCorner.png';
+import OurServicePc from '/assets/Image/OurServicePc.png';
+import PcChair from '/assets/Image/PcChair.png';
+import Standing from '/assets/Image/Standing.png';
+import Status from '/assets/Image/Status.png';
+import VenAI from '/assets/Image/VenAI.png';
+
+
+export {
+  Iphone1,
+  Iphone2,
+  Iphone2_2,
+  BillBoard,
+  BookStack,
+  BrandStack,
+  Buddy,
+  BusStop,
+  Cabin,
+  CallPc,
+  Car,
+  Cards,
+  Cat,
+  Cat2,
+  Cat3,
+  Celero,
+  ComicalEstate,
+  Desk,
+  DoubleQuote,
+  GridTypography,
+  GuidlinesStack,
+  HatypoMobileWhite,
+  HatypoPc,
+  HatypoPc2,
+  HoldPhone,
+  Homical,
+  HomicalLogo,
+  IdCard,
+  IphoneCorner,
+  OurServicePc,
+  PcChair,
+  Standing,
+  Status,
+  VenAI
+};
